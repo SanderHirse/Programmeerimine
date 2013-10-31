2d845ae540ef30a7af755279687ed202c3cdf987
SanderHirse <sanderhirse@gmail.com>
2013-10-31 13:47:15 +0200

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,47 +1,52 @@
-print "Tere!"
+text = "Esimene kysimus"
+print "", text.center(50, "=")
 
-tekst = raw_input('Kirjuta number!  ');
-if tekst.isdigit():
-	print "Tubli " + tekst
+teksti = raw_input('Sisesta tyhik! ');
+if teksti.isspace():
+        print "Tubli "
 else:
-	print "Proovi uuesti!"
-	uuesti = raw_input('Kirjuta palun number!  ');
-
-
-print "Tere!"
-
-rebane = raw_input('Mis häält teeb rebane? ');
-
-print rebane.lower();
-
-koer = raw_input('Mis häält teeb koer? ');
-
-print koer.lower();
-
-
-print "Tere!"
-
-tekst = raw_input('Kirjuta number!  ');
-if tekst.isdigit():
-	print "Tubli " + tekst
-else:
-	print "Proovi uuesti!"
-	uuesti = raw_input('Kirjuta palun number!  ');
-	if uuesti.isdigit():
-		print "Vapustav! " + uuesti
+        print "Proovi uuesti!"
+        uuestii = raw_input('Sisesta tyhik! ');
+	if uuestii.isspace():
+		print "V2ga tubli!"
 	else:
-		print "Ei, pidid kirjutama numbri!"
+		print "Tyhiks on k6ige pikem klahv sinu klaviatuuril!"
 
+text = "Teine kysimus"
+print "", text.center(50, "=")
 
-print "Tere!"
+rebane = raw_input('Mis h22lt teeb rebane? ');
 
-nimi = raw_input('Kes sõi vanaema moosi ära? ');
+print "rebane teeb " + rebane.lower();
 
-print nimi.capitalize();
+koer = raw_input('Mis h22lt teeb koer? ');
 
+print "Koer teeb " + koer.lower(); 
 
-print "Tere!"
+text = "Kolmas kysimus"
+print "", text.center(50, "=")
 
-tekst = raw_input('Kirjuta midagi suurte tähtedega!  ');
+tekst = raw_input('Kirjuta number! ');
+if tekst.isdigit():
+        print "Tubli " + tekst
+else:
+        print "Proovi uuesti!"
+        uuesti = raw_input('Kirjuta palun number! ');
+        if uuesti.isdigit():
+                print "Vapustav! " + uuesti
+        else:
+                print "Ei, oli vaja kirjutada number !"
 
-print tekst.lower()();
+text = "Neljas kysimus"
+print "", text.center(50, "=")
+
+nimi = raw_input('Kes s6i vanaema moosi 2ra? ');
+
+print "Vanaema moosi s6i 2ra" + nimi.capitalize();
+
+text = "Viies kysimus"
+print "", text.center(50, "=")
+
+tekst = raw_input('Kirjuta midagi suurte t2htedega! ');
+
+print tekst.lower();
