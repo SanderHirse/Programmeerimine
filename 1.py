223f70a1d92ab04fe6d7743fabd637a4da8481f1
SanderHirse <sanderhirse@gmail.com>
2013-10-31 11:03:01 +0200

    Algise pirnid

--- 1.py
+++ 1.py
@@ -1,1 +1,15 @@
-print "tere"
+print "Tere!"
+pirni = raw_input('Mitu pirni oli Algisel? ')
+string == pirni.isdigit()
+if string != true:
+        pirni = raw_input('Mitu pirni oli Algisel? (Number) ')
+print "Algisel on " + pirni + " pirni, "
+
+        
+  
+
+
+
+
+
+
