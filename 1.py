c7ea3c6eb68bf6cfda23d63a4bccc4698158dcaf
SanderHirse <sanderhirse@gmail.com>
2013-10-31 11:15:24 +0200

    tubli algis

--- 1.py
+++ 1.py
@@ -4,12 +4,7 @@ string == pirni.isdigit()
 if string != true:
         pirni = raw_input('Mitu pirni oli Algisel? (Number) ')
 print "Algisel on " + pirni + " pirni, "
-
-        
-  
-
-
-
-
-
-
+if pirni <= 2:
+        print "Algisel on " + pirni + " pirni ja ta on 6nnetu"
+        else:
+                print "Algisel on " + pirni + " ta andis valijatele 2 pirni ja nyyd on tal j2i alles " + pirni-2 + " pirni, nüüd on algis väga õnnelik!"
