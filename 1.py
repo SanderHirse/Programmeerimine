90423714b154959f6b883028137b7d2245c4d280
SanderHirse <sanderhirse@gmail.com>
2013-10-31 13:29:30 +0200

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,5 +1,47 @@
 print "Tere!"
 
-auto_nimi = raw_input('Mis on sinu auto numbrimärk = ');
+tekst = raw_input('Kirjuta number!  ');
+if tekst.isdigit():
+	print "Tubli " + tekst
+else:
+	print "Proovi uuesti!"
+	uuesti = raw_input('Kirjuta palun number!  ');
 
-print auto_nimi.upper();
+
+print "Tere!"
+
+rebane = raw_input('Mis häält teeb rebane? ');
+
+print rebane.lower();
+
+koer = raw_input('Mis häält teeb koer? ');
+
+print koer.lower();
+
+
+print "Tere!"
+
+tekst = raw_input('Kirjuta number!  ');
+if tekst.isdigit():
+	print "Tubli " + tekst
+else:
+	print "Proovi uuesti!"
+	uuesti = raw_input('Kirjuta palun number!  ');
+	if uuesti.isdigit():
+		print "Vapustav! " + uuesti
+	else:
+		print "Ei, pidid kirjutama numbri!"
+
+
+print "Tere!"
+
+nimi = raw_input('Kes sõi vanaema moosi ära? ');
+
+print nimi.capitalize();
+
+
+print "Tere!"
+
+tekst = raw_input('Kirjuta midagi suurte tähtedega!  ');
+
+print tekst.lower()();
