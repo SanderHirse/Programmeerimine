f83bc849081f78f08b70fa8d93136fc868bacb2f
SanderHirse <sanderhirse@gmail.com>
2013-10-31 13:01:48 +0200

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,10 +1,5 @@
 print "Tere!"
-pirni = raw_input('Mitu pirni oli Algisel? ')
-string == pirni.isdigit()
-if string != true:
-        pirni = raw_input('Mitu pirni oli Algisel? (Number) ')
-print "Algisel on " + pirni + " pirni, "
-if pirni <= 2:
-        print "Algisel on " + pirni + " pirni ja ta on 6nnetu"
-        else:
-                print "Algisel on " + pirni + " ta andis valijatele 2 pirni ja nyyd on tal j2i alles " + pirni-2 + " pirni, nüüd on algis väga õnnelik!"
+
+auto_nimi = raw_input('Mis on sinu auto numbrimärk = ');
+
+print auto_nimi.upper();
