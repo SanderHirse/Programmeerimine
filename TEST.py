67930d34bd347175151bb3570167fe736e509341
Sander Hirse <sanderhirse@gmail.com>
2013-11-06 13:59:44 +0200

    Create TEST.py

---
+++ TEST.py
@@ -0,0 +1,48 @@
+
+#print "RUUMALA"
+#x = 10
+#y = 20
+#z = 5
+#ruumala = x * y * z
+#print ruumala
+
+
+#print "uus".center(50, "=")
+
+#x = raw_input('pikkus: ')
+#y = raw_input('laius: ')
+#z = raw_input('Korgus: ')
+
+#print "RUUMALA"
+#x = int(x)
+#y = int(y)
+#z = int(z)
+#ruumala = x * y * z
+#print "{}".format(ruumala)+" "+"{}".format("Kuupsentimeetrit")
+
+
+print "matemaatika".center(50, "=")
+
+import math
+
+print "X = 50 "
+x = 50
+print math.ceil(x), ".ceil"
+print math.fabs(x), ".fabs"
+print math.floor(x), ".floor"
+print math.exp(x), ".exp"
+print math.log(x, 5), ".log"
+print math.log10(x), ".log10"
+print math.sqrt(x), ".sqrt"
+x = math.cos(x), ".cos"
+print math.acos(x), ".acos"
+print math.asin(x), ".asin"
+print math.atan(x), ".atan"
+x = 50
+print math.cos(x), ".cos"
+print math.sin(x), ".sin"
+print math.tan(x), ".tan"
+print math.degrees(x), ".degrees"
+print math.radians(x), ".radians"
+print math.pi, ".pi"
+print math.e, ".e"
