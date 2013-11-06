d1a53342d1a0f427bf4b683e3c280001d09c7abb
SanderHirse <sanderhirse@gmail.com>
2013-11-06 10:51:06 +0200

    Create 3-uudiskiri.py

---
+++ 3-uudiskiri.py
@@ -0,0 +1,12 @@
+u = "ALGIS SUURKIVI VALITI AINULT KAHE KANEPI KODANIKU POOLT"
+t1 = "Algis Suurkivi, Kanepi valla noor lootus"
+t2 = "on vedanud alt tervet Eesti vabariiki ning"
+t3 = "reformierakonda eesotsas Ansipiga!"
+
+
+print u.center(100, '-')
+print " "
+print " "
+print t1
+print t2
+print t3
