b47f84a006fa04365caaf8090b7ae0d070b1fb65
Sander Hirse <sanderhirse@gmail.com>
2013-11-13 10:45:58 +0200

    Update 05_matemaatika.py

--- 05_matemaatika.py
+++ 05_matemaatika.py
@@ -1,3 +1,4 @@
+import math
 vanus = 23
 print vanus
 vanus = 23 + 1
@@ -5,8 +6,21 @@ print vanus
 
 
 print "RUUMALA"
-x = 10
-Y = 20
-z = 5
+x = 5
+Y = 2
+z = 1
 ruumala = x * y * z
 print ruumala
+
+
+
+x = 65/math.sin(20 * math.pi/180)
+print 'vastus: x = ',round(x,2),'m';
+
+
+
+num = raw_input("Kirjutage number palun ")
+nump = float(num)
+print "Tangents: ",math.tan(nump * math.pi/180)
+print "Cossiinus: ",math.cos(nump * math.pi/180)
+print "Siinus: ",math.sin(nump * math.pi/180)
