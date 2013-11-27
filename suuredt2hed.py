f9c5a24ace5b69f001d52aeea1aebc94a8914eb4
Sander Hirse <sanderhirse@gmail.com>
2013-11-27 11:59:55 +0200

    Update suuredt2hed.py

--- suuredt2hed.py
+++ suuredt2hed.py
@@ -1,8 +1,7 @@
 t = raw_input('Sisesta tekst! ')
-if t != t.upper() and 1 == t.isalpha():
+if t.islower() and 1 == t.isalpha():
 	print "V2iket2hed, numbriteta"
 elif t != t.upper() and 0 == t.isalpha():
 	print "V2iket2hed, numbrid"
 else:
 	print "suuredt2hed"
-
